2fdb7beb22688a4d6480136dfd043d8c9d30e904
Pavan Varma <varmmsp110@outlook.com>
2016-11-30 13:35:58 +0530

    Add new programs

---
+++ 01 - Introduction/01 - Getting Started, The Easy Problems/01 - Super Easy/01124 - Celebrity jeopardy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    while (scanf("%c", &c) != EOF)
+        printf("%c", c);
+    return 0;
+}
+
+//Date: 30 Nov, 2016
+//Author: bumpy -_-
