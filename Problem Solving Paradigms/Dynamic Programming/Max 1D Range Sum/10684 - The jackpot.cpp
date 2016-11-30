d5be790f4e8704a296acc88d08a04d4fe0c0ce27
Pavan Varma <varmmsp110@outlook.com>
2016-11-30 11:48:30 +0530

    Add new problem

---
+++ Problem Solving Paradigms/Dynamic Programming/Max 1D Range Sum/10684 - The jackpot.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    while (scanf("%d", &n) && n) {
+        int sum = 0, res = 0;
+        int money;
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", &money);
+            sum += money;
+            res = max(res, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        if (res > 0)
+            printf("The maximum winning streak is %d.\n", res);
+        else
+            printf("Losing streak.\n");
+    }
+}
+
+//Date: 30 Nov, 2016
+//Author: bumpy -_-
