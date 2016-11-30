6b1ee89a4ea241693cb226e01a6a828f3df09340
Pavan Varma <varmmsp110@outlook.com>
2016-11-30 10:54:29 +0530

    Add new problems

---
+++ Problem Solving Paradigms/Dynamic Programming/Max 1D Range Sum/00507 - Jill Rides Again.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <algorithm>
+
+int main() {
+    int tt, s, niceness, route = 1;
+    scanf("%d", &tt);
+    while (tt--) {
+        int sum = 0, res = 0;
+        int temp = 1, start, stop;
+        scanf("%d", &s);
+        for (int i = 1; i < s; ++i) {
+            scanf("%d", &niceness);
+            sum += niceness;
+            if (sum < 0) {
+                sum = 0;
+                temp = i + 1;
+                continue;
+            }
+            if (sum > res) {
+                res = sum;
+                start = temp;
+                stop = i + 1;
+                continue;
+            }
+            if (sum == res && (i - temp + 1) > (stop - start)) {
+                start = temp;
+                stop = i + 1;
+                continue;
+            }
+        }
+        if (res > 0)
+            printf("The nicest part of route %d is between stops %d and %d\n", route, start, stop);
+        else
+            printf("Route %d has no nice parts\n", route);
+        ++route;
+    }
+}
+
+//Date: 30 Nov, 2016
+//Author: Bumpy -_-
