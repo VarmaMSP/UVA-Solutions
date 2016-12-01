f43befe95d400909af3f599918b09874a5985fdd
Pavan Varma <varmmsp110@outlook.com>
2016-12-01 20:30:25 +0530

    Add new problem

---
+++ 03 - Problem Solving Paradigms/02 - Dynamic Programming/03 - Longest Increasing Subsequence (LIS)/00111 - History Grading.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int order[24];
+int event[24], LIS[24];
+
+int main() {
+    int n, tmp;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", order + i);
+    }
+    while (scanf("%d", &tmp) != EOF) {
+        event[tmp] = 1;
+        for (int i = 2; i <= n; ++i) {
+            scanf("%d", &tmp);
+            event[tmp] = i;
+        }
+        for (int i = 1; i <= n; ++i) {
+            int tmp = 0;
+            for (int j = 1; j < i; ++j) {
+                if (order[event[j]] < order[event[i]] && LIS[j] > tmp)
+                    tmp = LIS[j];
+            }
+            LIS[i] = tmp + 1;
+        }
+        printf("%d\n", *max_element(LIS + 1, LIS + n + 1));
+    }
+    return 0;
+}
+
+//Date: 1 Dec, 2016
+//Author: bumpy -_-
