ea4d50b7caf4a455add0ef6b73c4f5c51a8468eb
varma msp <varma msp>
2016-12-07 23:38:58 +0530

    Add new problem

---
+++ 09 - Rare Topics/02 - Rare Problems/04 - Bracket Matching/00551 - Nesting a Bunch of Brackets.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map< char, char > bracketMatch;
+
+void init() {
+    bracketMatch['('] = ')';
+    bracketMatch['['] = ']';
+    bracketMatch['{'] = '}';
+    bracketMatch['<'] = '>';
+    bracketMatch['A'] = 'B'; //(* *)
+}
+
+inline bool isValidOpenBracket(char t) {
+    return  t == '(' || t == '[' || t == '{' ||
+            t == '<' || t == 'A';
+}
+
+inline bool isValidClosedBracket(char t) {
+    return  t == ')' || t == ']' || t == '}' ||
+            t == '>' || t == 'B';
+}
+
+int main() {
+    init();
+    char str[3050];
+    while (gets(str)) {
+        stack< char > S;
+        int len = strlen(str);
+        int pos = -1;
+        int cnt = 0;
+        bool isBalanced = true;
+        for (int i = 0; i < len; ++i, ++cnt) {
+            char tmp = str[i];
+            if (i < len - 1 && tmp == '(' && str[i + 1] == '*') {
+                tmp = 'A'; // (*
+                i += 1;
+            }
+            if (i < len - 1 && tmp == '*' && str[i + 1] == ')') {
+                tmp = 'B'; // *)
+                i += 1;
+            }
+            if (isValidOpenBracket(tmp)) {
+                S.push(tmp);
+                continue;
+            }
+            if (isValidClosedBracket(tmp)) {
+                pos = cnt + 1;
+                if (!S.empty() && bracketMatch[S.top()] == tmp) {
+                    S.pop();
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+        if (isBalanced && !S.empty()) {
+            isBalanced = false;
+            pos = cnt + 1;
+        }
+        if (isBalanced)
+            printf("YES\n");
+        else
+            printf("NO %d\n", pos);
+    }
+    return 0;
+}
+
+/*
+Author: bumpy (-_-)
+date: 7-Dec-2016
+*/
