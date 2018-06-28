9b96fa541c61931044955d18634e88b3f2242c49
siva pavan <siva.pavan@juspay.in>
2018-06-28 10:55:03 +0530

    Add new problem solution.

---
+++ 01 - Introduction/01 - Getting Started, The Easy Problems/01 - Super Easy/11044 - Searching for Nessy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int tt, m, n, sonars;
+    scanf("%d", &tt);
+    while (tt--) {
+      scanf("%d %d", &m, &n);
+      m -= 2; n -= 2;
+      sonars = m / 3 + (m % 3 ? 1 : 0);
+      sonars *= n / 3 + (n % 3 ? 1 : 0);
+      printf("%d\n", sonars);
+    }
+    return 0;
+}
+
+/*
+Author: bumpy (-_-)
+date: 28-Jun-2018
+*/
